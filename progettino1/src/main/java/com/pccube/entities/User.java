7da8f7ffdb75e6b2f8485112b35efa635d9a324d
PaolaElisabetta <PaolaElisabetta@AmeliaPond>
2018-10-04 11:22:00 +0200

    Added class User

---
+++ progettino1/src/main/java/com/pccube/entities/User.java
@@ -0,0 +1,63 @@
+package com.pccube.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Id
+	@Column
+	private String userName;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String type;
+	
+	@OneToMany
+	private List<Task> tasks = new ArrayList<>();
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	
+}
